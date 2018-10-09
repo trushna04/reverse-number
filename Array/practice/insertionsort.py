6416002fc9aaed73e5e816c8e3d56ae65c2bb44c
trushna4 <trushna4patel@gmail.com>
2018-10-09 16:15:52 -0400

    new

--- Array/practice/insertionsort.py
+++ Array/practice/insertionsort.py
@@ -1,16 +1,19 @@
 from typing import List, Any, Union
 
 from pip._vendor.distlib.compat import raw_input
+n= int(raw_input())
+a= []
+e=0
+for i in range (n):
+    b=int(raw_input())
+    a.append(b)
+for e in range (len ( a ) ):
+    for j in range (e+1, len ( a ) ):
+        if(a[e]>a[j]):
+            key=a[j]
+            a[j]=a[e]
+            a[e]=key
 
-a = [12, 11, 13, 5, 6]
-i=0
-j=i+1
-for j in range (1, len ( a ) ):
-    key = a[j]
-    a[i] = a[j] - 1;
-while (i > 0 and a[i] > key):
-    a[i + 1] = a[i]
-    i = i - 1
-    a[i + 1] = key
-    i=i+1;
+
 print ( a )
+
